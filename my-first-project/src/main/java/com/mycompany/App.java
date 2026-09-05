76aa48c4c2486ba25d64192cb9d479af00b5a74c
Saikat-1003 <saikat.g1996@gmail.com>
2026-09-05 16:04:04 +0530

    Build docker image without using jib plugin in maven

---
+++ my-first-project/src/main/java/com/mycompany/App.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("   Maven + Docker + Git Application");
+        System.out.println("================================");
+        System.out.println("Hello from inside Docker!");
+	System.out.println(" \n This is a handson test project for learning purposes\n   ");
+    }
+}
